5874f235ad3e4808efb10592ba88f6a7140987f7
Patrick Epstein <Patrick.Epstein@zuehlke.com>
2016-09-23 19:55:26 +0200

    Created game creation view

--- wedding-quiz/src/game/create.js
+++ wedding-quiz/src/game/create.js
@@ -1,5 +1,63 @@
 export class create {
     constructor() {
         this.message = 'Hello World!';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+        this.questions = [
+            'Wer geht am Sonntag gern zum Bäcker?',
+            'Wer wäscht ab?',
+            'Wer räumt auf?',
+            'Wer macht die Betten?',
+            'Wer telefoniert häufiger mit Mutti?',
+            'Wer treibt mehr Sport?',
+            'Wer hat den besser durchtrainierten Körper?',
+            'wer benötigt am Bad immer am längsten?',
+            'Wer ist pünktlicher?',
+            'Wer kommt immer zu spät?',
+            'Wer ist lauter beim Sex?',
+            'Wer hat mehr Lust auf Sex?',
+            'Wer kann besser verlieren?',
+            'Wer kann besser gewinnen?',
+            /*'Wer ist der Partylöwe?',
+            'Wer trinkt mehr?',
+            'Wer redet mehr?',
+            'Wer ist eifersüchtiger?',
+            'Wer liebt mehr?',
+            'Wer kann besser Entscheidungen fällen?',
+            'Wer gibt gerne Geld aus?',
+            'Wer ist der Genießer?',
+            'Wer verführt mehr?',
+            'Wer tat den ersten Schritt beim Kennenlernen?',
+            'Wer hat die Initiative beim ersten Kuss getroffen?',
+            'Wer sorgt für die Verhütung?',
+            'Wer kann zickiger sein?',
+            'Wer kann besser nachgeben?',
+            'Wer versucht öfters zu streiten?',*/
+            'Wer schläft länger?'];
     }
 }
