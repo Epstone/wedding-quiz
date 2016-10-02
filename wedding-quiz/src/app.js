a75a4e8b54031d32f00319fd74fdab8562ea57b8
Epstone <epstone2@gmail.com>
2016-10-02 12:27:21 +0200

    Added lobby & edited join game

--- wedding-quiz/src/app.js
+++ wedding-quiz/src/app.js
@@ -11,7 +11,8 @@ export class App {
             { route: ['/game/create'], name: 'game', moduleId: 'game/create' },
             { route: ['/game/join'], name: 'game', moduleId: 'game/join' },
             { route: ['/question/answer'], name: 'question', moduleId: 'question/answer' },
-            { route: ['/highscore'], name: 'highscore', moduleId: 'highscore/highscore' }
+            { route: ['/highscore'], name: 'highscore', moduleId: 'highscore/highscore' },
+            { route: ['/lobby'], name: 'lobby', moduleId: 'lobby/lobby' }
             // { route: 'users', name: 'users', moduleId: 'users/index', nav: true },
             // { route: 'users/:id/detail', name: 'userDetail', moduleId: 'users/detail' },
             // { route: 'files*path', name: 'files', moduleId: 'files/index', href: '#files', nav: true }
