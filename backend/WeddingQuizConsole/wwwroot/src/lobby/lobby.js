a881647e19eac48ac9e25f5fe96753a571c874ed
Patrick Epstein <Patrick.Epstein@zuehlke.com>
2017-03-19 20:09:44 +0100

    introduced event aggregator

--- backend/WeddingQuizConsole/wwwroot/src/lobby/lobby.js
+++ backend/WeddingQuizConsole/wwwroot/src/lobby/lobby.js
@@ -1,34 +1,31 @@
-import {signalrService} from 'services/signalr-service';
-import {inject} from 'aurelia-framework';
+import { signalrService } from 'services/signalr-service';
+import { inject } from 'aurelia-framework';
+import { EventAggregator } from 'aurelia-event-aggregator';
 
-@inject(signalrService)
+@inject(signalrService, EventAggregator)
 export class join {
-    constructor(signalrService) {
+    constructor(signalrService, eventAggregator) {
         console.log("lobby.js: received signalr service in constructor")
         this.message = 'Hello World!';
         this.playerlist = ["Noch keine Spieler"];
+        this.signalrService = signalrService;
+        this.eventAggregator = eventAggregator;
     }
 
     activate(joinDetails) {
         var self = this;
         console.log("welcome to the lobby");
+
         var game = joinDetails.game;
         var username = joinDetails.username;
 
-        var gameHub = $.connection.hub.createHubProxy("postsHub");
-        $.connection.hub.qs = 'username=' + username;
-        
-        gameHub.on('playerListUpdated', function (updatedPlayerlist) {
-            console.log("Received playerlist");
-            console.log(updatedPlayerlist);
-            self.playerlist = updatedPlayerlist;
-        });
-
-        $.connection.hub.logging = true;
-        $.connection.hub.start().done(function () {
-            console.log("hub is started now.");
-            gameHub.invoke('updatePlayerList');
-        });
-
+        this.signalrService.verifyConnected(username)
+            .then(() => {
+                this.eventAggregator.subscribe('playerListUpdated', (updatedPlayerList) => {
+                    console.log("we should update playerlist now. yes works")
+                    console.log(updatedPlayerList);
+                    self.playerlist = updatedPlayerList;
+                })
+            });
     }
 }
--- backend/WeddingQuizConsole/wwwroot/src/services/signalr-service.js
+++ backend/WeddingQuizConsole/wwwroot/src/services/signalr-service.js
@@ -1,11 +1,46 @@
+import { inject } from 'aurelia-framework';
+import { EventAggregator } from 'aurelia-event-aggregator';
+
+@inject(EventAggregator)
 export class signalrService {
-  constructor() {
+  constructor(eventAggregator) {
     console.log("signalr service constructor created")
+    this.eventAggregator = eventAggregator;
   }
 
   activate(params) {
     console.log("signalr service activated")
+
+  }
+
+  verifyConnected(username) {
+    var self = this;
+
+    var promise = new Promise(
+      (resolve, reject) => {
+
+        var gameHub = $.connection.hub.createHubProxy("postsHub");
+        $.connection.hub.qs = 'username=' + username;
+        $.connection.hub.logging = true;
+
+        gameHub.on('playerListUpdated', function (updatedPlayerlist) {
+          console.log("Received playerlist");
+          console.log(updatedPlayerlist);
+          self.eventAggregator.publish('playerListUpdated', updatedPlayerlist);
+        });
+
+
+
+        $.connection.hub.start().done(function () {
+          resolve();
+          console.log("hub is started now.");
+          gameHub.invoke('updatePlayerList');
+        });
+
+      }
+    );
+
+    return promise;
   }
 }
 
-  
