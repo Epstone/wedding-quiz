25bb554e74c1e41a81b4d0ebb2b6d79e540e5cb2
Patrick Epstein <Patrick.Epstein@zuehlke.com>
2017-03-19 20:15:22 +0100

    playerlist synchronized between views

--- backend/WeddingQuizConsole/wwwroot/src/game/create.js
+++ backend/WeddingQuizConsole/wwwroot/src/game/create.js
@@ -1,13 +1,30 @@
-export class create {
-    
-    questions = [];
+import { signalrService } from 'services/signalr-service';
+import { inject } from 'aurelia-framework';
+import { EventAggregator } from 'aurelia-event-aggregator';
 
-    constructor() {
+@inject(signalrService, EventAggregator)
+export class create {
+    constructor(signalrService, eventAggregator) {
         this.message = 'Hello World!';
+        this.playerlist = ["Noch keine Spieler"];
+        this.signalrService = signalrService;
+        this.eventAggregator = eventAggregator;
     }
 
+    questions = [];
+
     activate(params, routeData) {
+        var self = this;
         console.log("passed game from other view", params, routeData);
         this.questions = params.questions;
+
+        this.signalrService.verifyConnected("moderator")
+            .then(() => {
+                this.eventAggregator.subscribe('playerListUpdated', (updatedPlayerList) => {
+                    console.log("we should update playerlist now for moderator view.")
+                    console.log(updatedPlayerList);
+                    self.playerlist = updatedPlayerList;
+                })
+            });
     }
 }
