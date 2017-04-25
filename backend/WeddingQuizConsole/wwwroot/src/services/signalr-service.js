4e780e2e125a2727443712446da84eccfb4d0d3b
Patrick Epstein <Patrick.Epstein@zuehlke.com>
2017-04-25 18:39:24 +0200

    added game hub instance dependency

--- backend/WeddingQuizConsole/wwwroot/src/services/signalr-service.js
+++ backend/WeddingQuizConsole/wwwroot/src/services/signalr-service.js
@@ -1,5 +1,6 @@
 import { inject } from 'aurelia-framework';
 import { EventAggregator } from 'aurelia-event-aggregator';
+import { GameHumbInstance } from 'sevices/game-hub-instance';
 
 @inject(EventAggregator)
 export class SignalrService {
