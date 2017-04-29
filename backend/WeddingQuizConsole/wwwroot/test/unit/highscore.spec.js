4d57555a48aada21564a89c5165d9edc7f0e7450
Patrick Epstein <Patrick.Epstein@zuehlke.com>
2017-04-29 20:30:23 +0200

    try to test view and viewmodel

--- backend/WeddingQuizConsole/wwwroot/test/unit/highscore.spec.js
+++ backend/WeddingQuizConsole/wwwroot/test/unit/highscore.spec.js
@@ -1,30 +1,57 @@
 import { StageComponent } from 'aurelia-testing';
 import { bootstrap } from 'aurelia-bootstrapper';
-import { SignalrService } from 'services/signalr-service';
+import { GameHubSingleton } from 'services/game-hub-singleton';
+import {Promise} from 'bluebird';
+import { waitFor } from 'aurelia-testing';
 
-export class MockApi {
-    response = undefined;
+export class GameHubSingletonMock {
+    constructor() {
+        var self = this;
+        let onSubscriptions = {};
+        this.isInstanceCreated = false;
 
-    verifyConnected() {
-        console.log("verify connected was called :)");
-
-        var game = {
-            currentQuestionIndex: 0,
-            questions: ["A,B,C"],
-            highscore: [
-                { name: "Paul", score: 7 },
-                { name: "Matthias", score: 5 },
-                { name: "Arno", score: 3 },
-            ]
+        this.instanceMock = {
+            on: function (subscriptionName, callback) {
+                console.log("registered callback for - " + subscriptionName);
+                onSubscriptions[subscriptionName] = callback;
+            },
+            trigger: function (subscriptionName, params) {
+                console.log("triggering - " + subscriptionName, params);
+                onSubscriptions[subscriptionName](params);
+            }
         };
+    }
 
-        return Promise.resolve(game);
+    createGameHub() {
+        this.isInstanceCreated = true;
+        return this.instance;
+    }
+
+    get instance() {
+        if (this.isInstanceCreated) {
+            return this.instanceMock;
+        } else {
+            return null;
+        }
     }
 }
 
+
+
+
 describe('MyComponent', () => {
     let component;
-    let api = new MockApi();
+    let api = new GameHubSingletonMock();
+
+    var game = {
+        currentQuestionIndex: 0,
+        questions: ["A", "B", "C"],
+        highscore: [
+            { name: "Paul", score: 7 },
+            { name: "Matthias", score: 5 },
+            { name: "Arno", score: 3 },
+        ]
+    };
 
     beforeEach(() => {
         component = StageComponent
@@ -33,10 +60,8 @@ describe('MyComponent', () => {
             .boundTo({ currentQuestion: 'Wer wäscht ab?' });
 
         component.bootstrap(aurelia => {
-            aurelia.use
-                .standardConfiguration();
-
-            aurelia.container.registerInstance(SignalrService, api);
+            aurelia.use.standardConfiguration();
+            aurelia.container.registerInstance(GameHubSingleton, api);
         });
     });
 
@@ -44,10 +69,22 @@ describe('MyComponent', () => {
 
         component.create(bootstrap).then(() => {
             component.viewModel.activate({ gameId: "12345" });
-            const nameElement = document.querySelector("[data-test-id='heading']");
-            expect(nameElement.innerHTML).toBe('Übersicht');
-            done();
-            return;
+            const headingElement = document.querySelector("[data-test-id='heading']");
+            expect(headingElement.innerHTML).toBe('Übersicht');
+            api.instance.trigger("gameUpdated", game);
+
+            waitFor(() => {
+                const currentQuestionElement = document.querySelector("[data-test-id='current-question']");
+                if (currentQuestionElement.innerHTML == 'A') {
+                    return currentQuestionElement;
+                }
+                else {
+                    return null;
+                }
+            }).then((questionElement) => {
+                  expect(nameElement.innerHTML).toBe('A');
+                done();
+            });
         }).catch(e => {
             console.log(e.toString())
         });
@@ -57,4 +94,6 @@ describe('MyComponent', () => {
         component.dispose();
     });
 });
+
+
 
