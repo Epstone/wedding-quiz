d304a63e8d0f7dead0c39be49d312de428915fa9
Patrick Epstein <Patrick.Epstein@zuehlke.com>
2017-05-08 18:44:53 +0200

    rename #2

--- backend/MrAndMrs.Test/UiTestFixture.cs
+++ backend/MrAndMrs.Test/UiTestFixture.cs
@@ -16,7 +16,7 @@ namespace WeddingQuiz.Test
 
         public UiTestFixture()
         {
-            var serverExecutable = Directory.GetCurrentDirectory() + "\\..\\..\\..\\" + "MrAndMisses.App\\bin\\debug\\WeddingQuizConsole.exe";
+            var serverExecutable = Directory.GetCurrentDirectory() + "\\..\\..\\..\\" + "MrAndMrs.App\\bin\\debug\\WeddingQuizConsole.exe";
             var info = new ProcessStartInfo(serverExecutable);
             info.WindowStyle = ProcessWindowStyle.Minimized;
             webServer = Process.Start(info);
