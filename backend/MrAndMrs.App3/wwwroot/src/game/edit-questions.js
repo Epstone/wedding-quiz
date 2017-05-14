77a83f25eb297728b2150a6b53f94f7da2c63c3d
Patrick Epstein <Patrick.Epstein@zuehlke.com>
2017-05-14 17:08:01 +0200

    not working yet..

--- backend/MrAndMrs.App3/wwwroot/src/game/edit-questions.js
+++ backend/MrAndMrs.App3/wwwroot/src/game/edit-questions.js
@@ -2,38 +2,41 @@ export class EditQuestions {
   activate(questions) {
     var self = this;
     self.questions = questions;
+    self.questionsModel = [];
 
     for (var i = 0; i < self.questions.length; i++) {
-      self.questionsModel.push({
-        text: self.questions[i],
-        editActive: false,
-        editAction: self.changeEditState,
-        updateAction: (question) => self.applyQuestionUpdate(self, question)
-      });
+      self.questionsModel.push(new Question(self.questions[i]));
     }
-  }
-
-  changeEditState() {
-    this.editActive = !this.editActive;
   }
 
   addQuestion() {
     let self = this;
-    let questionToCreate = {
-      text: this.newQuestionText,
-      editActive: false,
-      editAction: this.changeEditState
-    };
-
+    let questionToCreate = new Question(this.newQuestionText);
     this.questionsModel.push(questionToCreate);
-
     this.updateQuestions();
   }
 
   applyQuestionUpdate(parent, child) {
-    child.editActive = false;
     parent.updateQuestions.apply(parent);
   }
+
+}
+
+export class Question {
+  constructor(text) {
+    this.text = text;
+    this.editActive = false;
+  }
+
+  changeEditState() {
+    this.editActive = true;
+  }
+
+  updateQuestions() {
+    this.editActive = false;
+    debugger;
+  }
+
 
 }
 
