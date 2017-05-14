0b83b42a856ba8f7f3f9e11e999c373185681a68
Patrick Epstein <Patrick.Epstein@zuehlke.com>
2017-05-14 16:51:22 +0200

    basic model delegation works

--- backend/MrAndMrs.App3/wwwroot/src/game/edit-questions.js
+++ backend/MrAndMrs.App3/wwwroot/src/game/edit-questions.js
@@ -1,6 +1,6 @@
-import { bindable } from 'aurelia-framework';
-
-export class EditQuestionsCustomElement {
-  @bindable model;
+export class EditQuestions {
+  activate(model) {
+    debugger;
+  }
 }
 
