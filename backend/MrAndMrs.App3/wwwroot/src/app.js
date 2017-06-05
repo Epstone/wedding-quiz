8b7a908102bb66d2bd56aca60daf47163001b864
Patrick Epstein <Patrick.Epstein@zuehlke.com>
2017-06-05 20:15:50 +0200

    added impressum und datenschutz

--- backend/MrAndMrs.App3/wwwroot/src/app.js
+++ backend/MrAndMrs.App3/wwwroot/src/app.js
@@ -12,7 +12,9 @@ export class App {
             { route: ['/game/join'], name: 'game', moduleId: 'game/join' },
             { route: ['/question/question'], name: 'question', moduleId: 'question/question' },
             { route: ['/highscore'], name: 'highscore', moduleId: 'highscore/highscore' },
-            { route: ['/lobby'], name: 'lobby', moduleId: 'lobby/lobby' }
+            { route: ['/lobby'], name: 'lobby', moduleId: 'lobby/lobby' },
+            { route: ['/impressum'], name: 'impressum', moduleId: 'home/impressum.html' },
+            { route: ['/datenschutz'], name: 'datenschutz', moduleId: 'home/datenschutz.html' }
             // { route: 'users', name: 'users', moduleId: 'users/index', nav: true },
             // { route: 'users/:id/detail', name: 'userDetail', moduleId: 'users/detail' },
             // { route: 'files*path', name: 'files', moduleId: 'files/index', href: '#files', nav: true }
